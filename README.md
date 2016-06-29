a201a6cc535516e47651deb6a47bccabd351c64c
Jacob Evans <jacob@dekz.net>
2016-06-29 21:55:32 +1000

    initial

---
+++ README.md
@@ -0,0 +1,6 @@
+# awsm profiles
+
+Support multiple AWS profiles in AWSM. Easily switch between Production and Non Production.
+
+[![asciicast](https://asciinema.org/a/3cov0pfdbwl4y9mksikiafxm4.png)](https://asciinema.org/a/3cov0pfdbwl4y9mksikiafxm4)
+
---
+++ awsm-profiles.sh
@@ -0,0 +1,53 @@
+#! /bin/bash 
+: ${AWS_CONFIG=$HOME/.aws/config}
+: ${AWSM_HOME=$HOME/.awsm}
+: ${AWSM_PROFILE=$AWSM_HOME/.awsm-profile}
+
+function list-profiles {
+  local regex="^\[profile ([a-z]+)\]"
+  cat "$AWS_CONFIG" | \
+  while read LINE; do
+    if [[ $LINE =~ $regex ]]
+    then
+      name="${BASH_REMATCH[1]}"
+      local match=$(list-current-profile)
+      if [[ $name == "$match" ]]
+      then
+        echo "$name *"
+      else
+        echo $name
+      fi
+    fi
+  done
+}
+
+function list-current-profile {
+  local regex="^export AWS_PROFILE=([a-z]+)"
+  cat "$AWSM_PROFILE" | \
+  while read LINE; do
+    if [[ $LINE =~ $regex ]]; then
+      name="${BASH_REMATCH[1]}"
+      echo $name
+    fi
+  done
+}
+
+function select-profile {
+  local profile=$(list-profiles | $FUZZY_FILTER)
+  if [ -n "$profile" ]; then
+    local regex="^([a-z]+)"
+    if [[ $profile =~ $regex ]]; then
+      name="${BASH_REMATCH[1]}"
+      echo $name
+    fi
+  fi
+}
+
+function profile {
+  local profile=$(select-profile)
+  if [ -n "$profile" ]; then
+    echo "export AWS_PROFILE=$profile" > $AWSM_PROFILE
+  fi
+}
+
+source $AWSM_PROFILE
